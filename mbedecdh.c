fbd96913774c1a1ce14c93c21809aaf45b289881
Zi-Shane <tshuang@smail.nchu.edu.tw>
2021-07-11 17:10:42 +0800

    add ecdh & change file name

---
+++ mbedecdh.c
@@ -0,0 +1,214 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "mbedtls/entropy.h"
+#include "mbedtls/ctr_drbg.h"
+#include "mbedtls/ecdh.h"
+#include "mbedtls/md.h"
+#include "mbedtls/hkdf.h"
+
+static void dump_buf( const char *title, unsigned char *buf, size_t len )
+{
+    size_t i;
+
+    printf( "%s", title );
+    for( i = 0; i < len; i++ )
+        printf("%c%c", "0123456789ABCDEF" [buf[i] / 16],
+                       "0123456789ABCDEF" [buf[i] % 16] );
+    printf( "\n" );
+}
+
+unsigned char* hexstr_to_char(const char* hexstr, size_t* olen)
+{
+    size_t len = strlen(hexstr);
+    if (len % 2 != 0)
+        return NULL;
+    size_t final_len = len / 2;
+    *olen = final_len;
+    unsigned char* chrs = (unsigned char*)malloc((final_len+1) * sizeof(*chrs));
+    for (size_t i=0, j=0; j<final_len; i+=2, j++)
+        chrs[j] = (hexstr[i] % 32 + 9) % 25 * 16 + (hexstr[i+1] % 32 + 9) % 25;
+    chrs[final_len] = '\0';
+    return chrs;
+}
+
+void getbytes(unsigned char* bytes, int value)
+{
+    bytes[0] = (value >> 24) & 0xFF;
+    bytes[1] = (value >> 16) & 0xFF;
+    bytes[2] = (value >> 8) & 0xFF;
+    bytes[3] = value & 0xFF;
+}
+
+int KDF(unsigned char* sharedZ);
+int getkey();
+
+int getkey()
+{
+    int ret = 1;
+
+    /*
+     * Random number generator
+     */
+    mbedtls_ctr_drbg_context ctr_drbg;
+    mbedtls_ctr_drbg_init( &ctr_drbg );
+    mbedtls_entropy_context entropy;
+    mbedtls_entropy_init( &entropy );
+    const char pers[] = "ecdh";
+    if( ( ret = mbedtls_ctr_drbg_seed( &ctr_drbg, mbedtls_entropy_func, &entropy,
+                               (const unsigned char *) pers,
+                               sizeof pers ) ) != 0 )
+    {
+        printf( " failed\n  ! mbedtls_ctr_drbg_seed returned -0x%04x\n", (unsigned int) -ret );
+
+    }
+
+    /*
+     * Assign party U's public key and party V's private key
+     */
+    mbedtls_ecdh_context ctx_srv;
+    mbedtls_ecdh_init( &ctx_srv );
+    
+    mbedtls_ecp_group_load( &ctx_srv.grp, MBEDTLS_ECP_DP_SECP256R1 );
+
+    // assign private key
+    if( ( ret = mbedtls_mpi_read_string(&ctx_srv.d, 16, "CE69EFE1E68415AD5B9F4C8B2F3025CB1332DDBD881073309A53A526FD3D7DBD") ) != 0 )
+    {
+        printf( " failed\n  ! mbedtls_mpi_read_string returned -0x%04x\n", (unsigned int) -ret );
+    }
+
+    // read public key of the peer
+    if( ( ret = mbedtls_ecp_point_read_string( &ctx_srv.Qp, 16, "BC316842562AB04CA987F39FBC5368899A0F2D6059E1247B68D3DC4F26C75669", "F808C6BB115B3B43E7F3A23D3E5F4BB3628183615A5604E1E603C9563BB24942" ) ) != 0 )
+    {
+        printf( " failed\n  ! mbedtls_ecp_point_read_string returned -0x%04x\n", (unsigned int) -ret );
+    }
+
+    // no need
+    // ret = mbedtls_mpi_lset( &ctx_srv.Qp.Z, 1 );
+    // if( ret != 0 )
+    // {
+    //     printf( " failed\n  ! mbedtls_mpi_lset returned %d\n", ret );
+    // }
+
+    /*
+     * save shared secret Z to mbedtls_mpi
+     */
+    // ret = mbedtls_ecdh_compute_shared( &ctx_srv.grp, &ctx_srv.z,
+    //                                    &ctx_srv.Qp, &ctx_srv.d,
+    //                                    mbedtls_ctr_drbg_random, &ctr_drbg );
+    // if( ret != 0 )
+    // {
+    //     printf( " failed\n  ! mbedtls_ecdh_compute_shared returned %d\n", ret );
+    // }
+
+    // unsigned char shared_secret_z[32];  // shared secret is 32 bytes
+    // if (mbedtls_mpi_write_binary(&ctx_srv.z, shared_secret_z, 32) != 0)
+    // {
+    //     printf( " failed\n  ! mbedtls_mpi_write_binary returned -0x%04x\n", (unsigned int) -ret );
+    // }
+    // dump_buf("  + Z: ", shared_secret_z, 32);
+
+    /*
+     * save shared secret Z to byte array
+     */
+    size_t olen;
+    unsigned char sharedZ[32];  // shared secret is 32 bytes
+    ret = mbedtls_ecdh_calc_secret( &ctx_srv, &olen, sharedZ, 32, mbedtls_ctr_drbg_random, &ctr_drbg );
+    if( ret != 0 )
+    {
+        printf( " failed\n  ! mbedtls_ecdh_calc_secret returned %d\n", ret );
+    }
+    dump_buf("  + Z: ", sharedZ, 32);
+
+    KDF(sharedZ);
+}
+
+int KDF(unsigned char* sharedZ)
+{
+    int ret;
+
+    /*
+     * KDF
+     *
+     * One-pass-DH
+     * otherInfo = ( AlgID || partyU's System Title || partyV's System Title )
+     * Static-DH
+     * otherInfo = ( AlgID || partyU's System Title || 0x08 + TransactionId || partyV's System Title )
+     */
+    // 6085740508030043545430303030304142434445464748
+    // 6085740508030043545430303030300836303739433330414142434445464748
+    unsigned char v[4];
+    size_t len_otherInfo;
+    unsigned char* otherInfo = hexstr_to_char("6085740508030043545430303030300836303739433330414142434445464748", &len_otherInfo);
+
+    size_t hashlen, keylen, reps;
+    const mbedtls_md_info_t *md_info;
+    mbedtls_md_context_t md_ctx;
+    mbedtls_md_init( &md_ctx );
+    md_info = mbedtls_md_info_from_type( MBEDTLS_MD_SHA256 );
+    if( ( ret = mbedtls_md_setup( &md_ctx, md_info, 0 ) ) != 0 )
+    {
+        printf( " failed\n  ! mbedtls_md_setup returned -0x%04x\n", (unsigned int) -ret );
+    }
+
+    // NIST.SP.800-56A KDF
+    hashlen = mbedtls_md_get_size( md_info );
+    keylen = 32; // ECDH with P-256, so result is 256 bits (32 bytes)
+    unsigned char key[keylen];
+    unsigned char hash_output[MBEDTLS_MD_MAX_SIZE];
+    reps = keylen/hashlen;
+
+    for (int pos = 1; pos <= reps; pos++)
+    {
+        // Calculate hash
+        if( ( ret = mbedtls_md_starts( &md_ctx ) ) != 0 )
+        {
+            printf( " failed\n  ! mbedtls_md_starts returned -0x%04x\n", (unsigned int) -ret );
+        }
+        // Add salt
+        getbytes(v, pos);
+        if( ( ret = mbedtls_md_update( &md_ctx, v, 4 ) ) != 0 )
+        {
+            printf( " failed\n  ! mbedtls_md_update returned -0x%04x\n", (unsigned int) -ret );
+        }
+        if( ( ret = mbedtls_md_update( &md_ctx, sharedZ, 32 ) ) != 0 )
+        {
+            printf( " failed\n  ! mbedtls_md_update returned -0x%04x\n", (unsigned int) -ret );
+        }
+        if( ( ret = mbedtls_md_update( &md_ctx, otherInfo, len_otherInfo ) ) != 0 )
+        {
+            printf( " failed\n  ! mbedtls_md_update returned -0x%04x\n", (unsigned int) -ret );
+        }
+        if( ( ret = mbedtls_md_finish( &md_ctx, hash_output ) ) != 0 )
+        {
+            printf( " failed\n  ! mbedtls_md_finish returned -0x%04x\n", (unsigned int) -ret );
+        }
+        // fill key length
+        if ( pos < reps )
+        {
+            memcpy( hash_output, key + hashlen * ( pos - 1 ), hashlen );
+        }
+        else
+        {
+            if ( keylen % hashlen == 0)
+            {
+                memcpy( key + hashlen * ( pos - 1 ), hash_output , hashlen );
+            }
+            else
+            {
+                memcpy( key + hashlen * ( pos - 1 ), hash_output , keylen % hashlen );
+            }
+        }
+    }
+
+    dump_buf("  + Key: ", key, 32);
+
+    printf( " ok\n" );
+}
+
+int main( int argc, char *argv[] )
+{
+    getkey();
+}
+
--- mbedecdsa.c
+++ mbedecdsa.c
@@ -50,16 +50,6 @@ static void dump_pubkey( const char *title, mbedtls_ecdsa_context *key )
 
 int sign() {
     int ret = 1;
-//     mbedtls_pk_context pk;
-//     mbedtls_pk_init( &pk );
-//     const unsigned char* mykey = "-----BEGIN PRIVATE KEY-----\n"\
-// "MIGHAgEAMBMGByqGSM49AgEGCCqGSM49AwEHBG0wawIBAQQgzmnv4eaEFa1bn0yLLzAlyxMy3b2IEHMwmlOlJv09fb2hRANCAAS8MWhCViqwTKmH85+8U2iJmg8tYFnhJHto09xPJsdWafgIxrsRWztD5/OiPT5fS7NigYNhWlYE4eYDyVY7sklC\n"\
-// "-----END PRIVATE KEY-----";
-
-//     if( ( ret = mbedtls_pk_parse_key( &pk, mykey, strlen(mykey)+1, NULL, 0 ) ) != 0 )
-//     {
-//         printf( " failed\n  ! mbedtls_pk_parse_key returned -0x%04x\n", (unsigned int) -ret );
-//     }
     mbedtls_entropy_context entropy;
     mbedtls_entropy_init( &entropy );
     mbedtls_ctr_drbg_context ctr_drbg;
@@ -74,8 +64,6 @@ int sign() {
 
     mbedtls_ecdsa_context ecdsaprikey;
     mbedtls_ecdsa_init(&ecdsaprikey);
-    // const mbedtls_ecp_keypair *pk_ecp = mbedtls_pk_ec(pk);
-    // mbedtls_ecdsa_from_keypair(&ecdsaprikey, pk_ecp);
 
     mbedtls_mpi_read_string(&ecdsaprikey.d, 16, "CE69EFE1E68415AD5B9F4C8B2F3025CB1332DDBD881073309A53A526FD3D7DBD");
     if( ( ret = mbedtls_ecp_group_load( &ecdsaprikey.grp, MBEDTLS_ECP_DP_SECP256R1 ) ) != 0 )
@@ -83,10 +71,6 @@ int sign() {
         printf( " failed\n  ! mbedtls_ecp_group_load returned -0x%04x\n", (unsigned int) -ret );
     }
 
-    mbedtls_mpi r;
-    mbedtls_mpi_init(&r);
-    mbedtls_mpi s;
-    mbedtls_mpi_init(&s);
     char text[] = "43545430303030304142434445464748073B0F45272556735A0A6663635F6853C7EDE9CA4154E6DEA172512FC1CCC8BAC0B883067C45160DAA12BF78DACD1838016F888F798BDD0D7826D0";
     unsigned char hashbyte[32];
     unsigned char *textbyte = hexstr_to_char(text);
@@ -98,6 +82,10 @@ int sign() {
 
     dump_buf( "  + Hash: ", hashbyte, sizeof(hashbyte) );
 
+    mbedtls_mpi r;
+    mbedtls_mpi_init(&r);
+    mbedtls_mpi s;
+    mbedtls_mpi_init(&s);
     /*
      * Computes the ECDSA signature
      * Deterministic version
@@ -116,39 +104,35 @@ int sign() {
         printf( " failed\n  ! mbedtls_ecdsa_sign returned -0x%04x\n", (unsigned int) -ret );
     }
 
-    char tmp[1000];
-    size_t olen;
-    mbedtls_mpi_write_string(&r, 16, tmp, sizeof(tmp), &olen);
-    printf("  + R: %s\n", tmp);
-    mbedtls_mpi_write_string(&s, 16, tmp, sizeof(tmp), &olen);
-    printf("  + S: %s\n", tmp);
+    // char tmp[1000];
+    // size_t olen;
+    // mbedtls_mpi_write_string(&r, 16, tmp, sizeof(tmp), &olen);
+    // printf("  + R: %s\n", tmp);
+    // mbedtls_mpi_write_string(&s, 16, tmp, sizeof(tmp), &olen);
+    // printf("  + S: %s\n", tmp);
 
-    // mbedtls_printf( " ok (signature length = %u)\n", (unsigned int) sig_len );
+    unsigned char signature_byte_r[32];
+    unsigned char signature_byte_s[32];
+    if (mbedtls_mpi_write_binary(&r, signature_byte_r, 32) != 0)
+    {
+        printf( " failed\n  ! mbedtls_mpi_write_binary returned -0x%04x\n", (unsigned int) -ret );
+    }
+    if (mbedtls_mpi_write_binary(&s, signature_byte_s, 32) != 0)
+    {
+        printf( " failed\n  ! mbedtls_mpi_write_binary returned -0x%04x\n", (unsigned int) -ret );
+    }
+    
+    dump_buf("  + R: ", signature_byte_r, 32);
+    dump_buf("  + S: ", signature_byte_s, 32);
 
-    // dump_buf( "  + Signature: ", sig, sig_len );
 }
 
 int verify() {
     int ret = 1;
-//     mbedtls_pk_context pk;
-//     mbedtls_pk_init( &pk );
-//     const unsigned char* myPubkey = "-----BEGIN PUBLIC KEY-----\n"\
-// "MFkwEwYHKoZIzj0CAQYIKoZIzj0DAQcDQgAEvDFoQlYqsEyph/OfvFNoiZoPLWBZ4SR7aNPcTybHVmn4CMa7EVs7Q+fzoj0+X0uzYoGDYVpWBOHmA8lWO7JJQg==\n"\
-// "-----END PUBLIC KEY-----";
-
-//     if( ( ret = mbedtls_pk_parse_public_key( &pk, myPubkey, strlen(myPubkey)+1 ) ) != 0 )
-//     {
-//         printf( " failed\n  ! mbedtls_pk_parse_public_keyfile returned -0x%04x\n", (unsigned int) -ret );
-//     }
 
     mbedtls_ecdsa_context ecdsapubkey;
     mbedtls_ecdsa_init(&ecdsapubkey);
-    // const mbedtls_ecp_keypair *keypair = mbedtls_pk_ec(pk);
-    // mbedtls_ecdsa_from_keypair(&ecdsapubkey, keypair);
-    // dump_pubkey( "  + Public key: ", &ecdsapubkey );
 
-    // mbedtls_ecp_point pubkey;
-    // mbedtls_ecp_point_init(&pubkey);
     if( ( ret = mbedtls_ecp_point_read_string( &ecdsapubkey.Q, 16, "bc316842562ab04ca987f39fbc5368899a0f2d6059e1247b68d3dc4f26c75669", "f808c6bb115b3b43e7f3a23d3e5f4bb3628183615a5604e1e603c9563bb24942" ) ) != 0 )
     {
         printf( " failed\n  ! mbedtls_ecp_point_read_string returned -0x%04x\n", (unsigned int) -ret );
@@ -174,12 +158,18 @@ int verify() {
 
     dump_buf( "  + Hash: ", hashbyte, sizeof( hashbyte ) );
 
+    char r_string[] = "DECE52910E62F5696082481FC22F60E29DD4E09B28CCF8D8FFCCAFE6C7699231";
+    char s_string[] = "893946FD46D67053DEB4EB046DE3EE8A5308D9C3CDEA89B49406FE8B14F51D2F";
+    unsigned char *r_byte = hexstr_to_char(r_string);
+    unsigned char *s_byte = hexstr_to_char(s_string);
     mbedtls_mpi r;
     mbedtls_mpi_init(&r);
-    mbedtls_mpi_read_string(&r, 16, "DECE52910E62F5696082481FC22F60E29DD4E09B28CCF8D8FFCCAFE6C7699231");
+    // mbedtls_mpi_read_string(&r, 16, "DECE52910E62F5696082481FC22F60E29DD4E09B28CCF8D8FFCCAFE6C7699231");
+    mbedtls_mpi_read_binary(&r, r_byte, 32);
     mbedtls_mpi s;
     mbedtls_mpi_init(&s);
-    mbedtls_mpi_read_string(&s, 16, "893946FD46D67053DEB4EB046DE3EE8A5308D9C3CDEA89B49406FE8B14F51D2F");
+    // mbedtls_mpi_read_string(&s, 16, "893946FD46D67053DEB4EB046DE3EE8A5308D9C3CDEA89B49406FE8B14F51D2F");
+    mbedtls_mpi_read_binary(&s, s_byte, 32);
     if( ( ret = mbedtls_ecdsa_verify(&ecdsapubkey.grp, hashbyte, 32, &ecdsapubkey.Q, &r, &s) ) != 0 )
     {
         printf( " failed\n  ! mbedtls_ecdsa_read_signature returned -0x%04x\n", (unsigned int) -ret );
@@ -190,6 +180,6 @@ int verify() {
 }
 
 int main() {
-    // sign();
-    verify();
+    sign();
+    // verify();
 }
